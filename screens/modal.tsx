ced269424c044b896818590150e8743773a795ce
urdadx <nerdyshinobi12@gmail.com>
2024-10-17 22:38:57 +0000

    setup project✨

---
+++ screens/modal.tsx
@@ -0,0 +1,13 @@
+import { ScreenContent } from 'components/ScreenContent'
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { Platform } from 'react-native'
+
+export default function Modal() {
+  return (
+    <>
+      <ScreenContent path="screens/modal.tsx" title="Modal" />
+      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+    </>
+  )
+}
